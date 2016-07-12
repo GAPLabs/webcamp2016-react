e73f74a1e11560d688f4a512c1c98e652793d222
Daisyrie Dumali <daisyriedumali@Daisyries-MacBook-Pro.local>
2016-07-12 14:15:46 +0800

    update

---
+++ js/threads.js
@@ -0,0 +1,43 @@
+var threads = [
+			 	{
+					vote: 0,
+					title: "Game of Thrones from scale of 1 to 10",
+					description: "In a scale of 1 to 10, how much do you know? -Jon Snow",
+					comments: [
+								{
+									name: "Jon Snow",
+									comment: "0; because I know nothing."
+								},
+								{
+									name: "Jon Snow",
+									comment: "0; because I know nothing."
+								}
+							  ]
+				 },
+				 {
+				 	vote: 0,
+					title: "Game of Thrones from scale of 1 to 10",
+					description: "In a scale of 1 to 10, how much do you ...",
+					comments: [
+								{
+									name: "Jon Snow",
+									comment: "0; because I know nothing."
+								}
+							  ]
+				 },
+				 {
+				 	vote: 0,
+					title: "Game of Thrones from scale of 1 to 10",
+					description: "In a scale of 1 to 10, how much do you ...",
+					comments: [
+								{
+									name: "Jon Snow",
+									comment: "0; because I know nothing."
+								},
+								{
+									name: "Jon Snow",
+									comment: "0; because I know nothing."
+								}
+							  ]
+				 }
+			 ];
