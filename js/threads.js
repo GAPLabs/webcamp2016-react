f6616d0a54dab23be33f3de62c365618509a0549
Daisyrie Dumali <daisyriedumali@daisy.local.gap>
2016-07-14 12:49:43 +0800

    merge styles and markup changes

--- js/threads.js
+++ js/threads.js
@@ -5,5 +5,10 @@ var threads = [{
 				description: "The FBI's sudden success in circumventing Apple's security measures might raise new questions about whether Apple's products are truly ironclad. It will also lead to heightened speculation over the \"outside party\" that assisted the government at the last minute and criticism from some quarters over the FBI's failure to hack into an iPhone on its own.",
 				submittedBy: 'mashable',
 				commentCount: 0,
-				comments: []
+				comments: [
+					{
+						name: "Nate Ruess",
+						comment: "I'm nothing without love chos"
+					}
+				]
 			  }];
