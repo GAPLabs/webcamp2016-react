3aaf4c7060424b6f7261ad14e1a685f1e0b59ea8
Daisyrie Dumali <daisyriedumali@daisy.local.gap>
2016-07-14 15:11:39 +0800

    threads.js

---
+++ js/threads.js
@@ -0,0 +1,8 @@
+var threads = [
+			 	{
+					vote: 0,
+					title: "Game of Thrones from scale of 1 to 10",
+					description: "In a scale of 1 to 10, how much do you know? -Jon Snow",
+					comments: []
+				 }
+			 ];
