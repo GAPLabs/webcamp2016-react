31464ad8e2d4ea810512e11c29ceaa8d32570400
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2016-07-15 09:36:21 +0800

    update

--- js/threads.js
+++ js/threads.js
@@ -1,28 +1,16 @@
 var threads = [
 				{
-					vote: 0,
-					title: "Game of Thrones from scale of 1 to 10",
-					description: "In a scale of 1 to 10, how much do you know? -Jon Snow",
+					rank: 1,
+					vote: 1,
+					title: 'Why playing Pokemon Go could cost you, big time',
+					description: 'In the interest of public information and safety, TheWrap reached out to the Los Angeles County Sheriff\'s Department to find out what the penalties of Pokemon GO-ing while driving. "The penalty for the infraction depends on previous driving convictions," Hill explained. "The first time penalty for cell phone violation is $20 and $50 for each subsequent offense."',
+					submittedBy: 'Nate Ruess',
+					commentCount: 0,
 					comments: [
-								{
-									name: "Jon Snow",
-									comment: "0; because I know nothing."
-								},
-								{
-									name: "Tommen Baratheon",
-									comment: "Por asdir aloer av vir io sdo  os"
-								}
-							]
-				 },
-				 {
-				 	vote: 0,
-					title: "Game of Thrones from scale of 1 to 10",
-					description: "In a scale of 1 to 10, how much do you ...",
-					comments: [
-								{
-									name: "Sansa Stark",
-									comment: "Lorem ipsum dolor sit amet"
-								}
-							]
-				 },
-			 ];
+						{
+							name: "Neil Alvarez",
+							comment: "I can assure you...you hit me while driving playing this game, the $150 ticket will be the least of your worries."
+						}
+					]
+			    }
+			  ];
